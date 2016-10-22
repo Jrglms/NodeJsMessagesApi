de474c41bb668a587c6c325ade0ed3b3bd73f2ae
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-10-22 18:55:18 +0100

    Added messagesManager
    Added manager class that will be in charge of adding and retrieving
    messages from the db.
    Added method to get global messages.

---
+++ common/managers/messagesManager.js
@@ -0,0 +1,14 @@
+﻿(function (messagesManager) {
+
+    var _logWriter = null;
+
+    messagesManager.init = function (logWriter) {
+
+        _logWriter = logWriter;
+    }
+
+
+        _logWriter.write("verbose", "Getting global conversations");
+    };
+
+})(module.exports)
