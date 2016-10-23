d7a86f6c3eefdddd310767b80256c9029a6a167a
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-10-23 12:36:17 +0100

    Split controller endpoints
    Routes changed.
    Added authentication function.

--- common/managers/messagesManager.js
+++ common/managers/messagesManager.js
@@ -14,16 +14,16 @@
         _logWriter.write("debug", "Not implemented yet.")
     };
 
-    messagesManager.getGroupMessages = function () {
+    messagesManager.getGroupMessages = function (groupId) {
 
-        _logWriter.write("debug", "Getting group messages...");
+        _logWriter.write("debug", "Getting group messages for group with Id '" + groupId + "'...");
 
         _logWriter.write("debug", "Not implemented yet.")
     };
 
-    messagesManager.getPrivateMessages = function () {
+    messagesManager.getPrivateMessages = function (requestingUserId, userId) {
 
-        _logWriter.write("debug", "Getting private messages...");
+        _logWriter.write("debug", "Getting private messages between users with Ids '" + requestingUserId + "' and ' " + userId + "'...");
 
         _logWriter.write("debug", "Not implemented yet.")
     };
--- server/controllers/messagesController.js
+++ server/controllers/messagesController.js
@@ -5,29 +5,44 @@
         var manager = require("common/managers/messagesManager");
         manager.init(logWriter)
 
-        app.get("/messages/:category", function (req, res) {
+        app.get("*", function (req, res, next) {
 
-            var category = req.params.category.toLowerCase();
+            logWriter.write("debug", "Authenticating user...");
 
-            logWriter.write("debug", "Getting messages with category '" + category + "'...");
+            logWriter.write("debug", "Work in progress. No authentication is required yet.");
 
-            switch (category) {
-                case "global":
-                    var messages = manager.getGlobalMessages();
-                    break;
-                case "group":
-                    var messages = manager.getGroupMessages();
-                    break;
-                case "private":
-                    var messages = manager.getPrivateMessages();
-                    break;
-                default:
-                    var errorMessage = "Category '" + category + "' not supported";
-                    logWriter.write("warning", errorMessage);
-                    res.send(errorMessage);
-                    return;
-            }
+            next();
+        })
+
+        app.get("/groups/:groupId/messages", function (req, res) {
+
+            var groupId = req.params.groupId.toLowerCase();
+
+            logWriter.write("debug", "Getting group messages for group with Id '" + groupId + "'...");
             
+            var messages = manager.getGroupMessages(groupId);
+
+            res.send(messages);
+        });
+
+        app.get("/users/:userId/messages", function (req, res) {
+
+            var requestingUserId = 0; // Get from header
+            var userId = req.params.userId.toLowerCase();
+
+            logWriter.write("debug", "Getting private messages between users with Ids '" + requestingUserId + "' and '" + userId + "'...");
+            
+            var messages = manager.getPrivateMessages(requestingUserId, userId);
+
+            res.send(messages);
+        });
+
+        app.get("/messages", function (req, res) {
+            
+            logWriter.write("debug", "Getting global messages...");
+            
+            var messages = manager.getGlobalMessages();
+
             res.send(messages);
         });
 
