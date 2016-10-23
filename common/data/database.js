05c1525f5d470e5008c39de4aec9b89b2a9881df
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-10-23 17:58:40 +0100

    Added class to talk with a database

---
+++ common/data/database.js
@@ -0,0 +1,23 @@
+﻿(function (database) {
+
+    var mongoDb = require("mongodb");
+    var mongoUrl = "mongodb://localhost:27017/MessagesApi";
+    var db = null;
+
+    database.getDb = function (next) {
+
+        if (!db) {
+            mongoDb.MongoClient.connect(mongoUrl, function (err, createdDb) {
+                if (err) {
+                    next(err, null);
+                } else {
+                    db = createdDb;
+                    next(null, db);
+                }
+            });
+        } else {
+            next(null, db);
+        }
+    }
+
+})(module.exports)
