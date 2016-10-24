2a0c2e0d21e666fa7ee02831c3cb1a6490c8aee0
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-10-24 20:07:15 +0100

    Tidied up the code

--- common/managers/messagesManager.js
+++ common/managers/messagesManager.js
@@ -12,129 +12,150 @@
 
         _logWriter.write("debug", "Adding global message...");
 
-        _conversationsRepository.findOneAndUpdate({ category: 1 }, { $push: { messages: { message: message, userId: userId } } }, { upsert: true }, function (err) {
-            if (err) {
-                _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
-                next(err);
-            }
-            else {
-                next(null);
-            }
-        });
+        _conversationsRepository.findOneAndUpdate(
+            { userIds: { $exists: false }, groupId: { $exists: false } }, // Query
+            { $push: { messages: { message: message, userId: userId } } }, // Projection
+            { upsert: true }, // Options
+            function (err) {
+                if (err) {
+                    _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
+                    next(err);
+                }
+                else {
+                    next(null);
+                }
+            });
     }
 
     messagesManager.addGroupMessage = function (userId, groupId, message, next) {
 
         _logWriter.write("debug", "Adding a new group message...");
 
-        _conversationsRepository.findOneAndUpdate({ category: 2, groupId: groupId }, { $push: { messages: { message: message, userId: userId } } }, { upsert: true }, function (err) {
-            if (err) {
-                _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
-                next(err);
-            }
-            else {
-                next(null);
-            }
-        });
+        _conversationsRepository.findOneAndUpdate(
+            { groupId: groupId }, // Query
+            { $push: { messages: { message: message, userId: userId } } }, // Projection
+            { upsert: true }, // Options
+            function (err) {
+                if (err) {
+                    _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
+                    next(err);
+                }
+                else {
+                    next(null);
+                }
+            });
     }
 
     messagesManager.addPrivateMessage = function (senderUserId, receiverUserId, message, next) {
 
         _logWriter.write("debug", "Adding a new private message...");
 
-        _conversationsRepository.findOneAndUpdate({ category: 3, userIds: [senderUserId, receiverUserId].sort(function (a, b) { return a - b; }) }, { $push: { messages: { message: message, userId: senderUserId } } }, { upsert: true }, function (err) {
-            if (err) {
-                _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
-                next(err);
-            }
-            else {
-                next(null);
-            }
-        });
+        _conversationsRepository.findOneAndUpdate(
+            { userIds: [senderUserId, receiverUserId].sort(function (a, b) { return a - b; }) }, // Query
+            { $push: { messages: { message: message, userId: senderUserId } } }, // Projection
+            { upsert: true }, // Options
+            function (err) {
+                if (err) {
+                    _logWriter.write("error", "Could not add new message. Error:\n" + "\t" + err);
+                    next(err);
+                }
+                else {
+                    next(null);
+                }
+            });
     }
 
     messagesManager.getGlobalMessages = function (next) {
 
         _logWriter.write("debug", "Getting global messages...");
 
-        _conversationsRepository.list({ category: 1 }, { messages: true }, function (err, results) {
-            if (err) {
-                _logWriter.write("error", "Failed to get global messages.\n" + "Error message:\n" + "\t" + err);
-                next(err, null);
-            } else {
-                switch (results.length) {
-                    case 0:
-                        _logWriter.write("debug", "No collection was found. Returning empty array.");
-                        next(null, []);
-                        break;
-                    case 1:
-                        _logWriter.write("debug", "The collection was found. Returning the associated messages.");
-                        next(null, results[0].messages);
-                        break;
-                    default:
-                        var errorMessage = "More than a collection of global messages was returned.";
-                        _logWriter.write("error", errorMessage);
-                        next(errorMessage, null);
-                        break;
+        _conversationsRepository.list(
+            { userIds: { $exists: false }, groupId: { $exists: false } }, // Query
+            { messages: true }, // Projection
+            function (err, results) {
+                if (err) {
+                    _logWriter.write("error", "Failed to get global messages.\n" + "Error message:\n" + "\t" + err);
+                    next(err, null);
+                } else {
+                    switch (results.length) {
+                        case 0:
+                            _logWriter.write("debug", "No collection was found. Returning empty array.");
+                            next(null, []);
+                            break;
+                        case 1:
+                            _logWriter.write("debug", "The collection was found. Returning the associated messages.");
+                            next(null, results[0].messages);
+                            break;
+                        default:
+                            var errorMessage = "More than a collection of global messages was returned.";
+                            _logWriter.write("error", errorMessage);
+                            next(errorMessage, null);
+                            break;
+                    }
                 }
-            }
-        });
+            });
     };
 
     messagesManager.getGroupMessages = function (groupId, next) {
 
         _logWriter.write("debug", "Getting group messages for group with Id '" + groupId + "'...");
         
-        _conversationsRepository.list({ category: 2, groupId: groupId }, { messages: true }, function (err, results) {
-            if (err) {
-                _logWriter.write("error", "Failed to get group messages.\n" + "Error message:\n" + "\t" + err);
-                next(err, null);
-            } else {
-                switch (results.length) {
-                    case 0:
-                        _logWriter.write("debug", "No collection was found. Returning empty array.");
-                        next(null, []);
-                        break;
-                    case 1:
-                        _logWriter.write("debug", "The collection was found. Returning the associated messages.");
-                        next(null, results[0].messages);
-                        break;
-                    default:
-                        var errorMessage = "More than a collection of group messages was returned.";
-                        _logWriter.write("error", errorMessage);
-                        next(errorMessage, null);
-                        break;
+        _conversationsRepository.list(
+            { groupId: groupId }, // Query
+            { messages: true }, // Projection
+            function (err, results) {
+                if (err) {
+                    _logWriter.write("error", "Failed to get group messages.\n" + "Error message:\n" + "\t" + err);
+                    next(err, null);
+                } else {
+                    switch (results.length) {
+                        case 0:
+                            _logWriter.write("debug", "No collection was found. Returning empty array.");
+                            next(null, []);
+                            break;
+                        case 1:
+                            _logWriter.write("debug", "The collection was found. Returning the associated messages.");
+                            next(null, results[0].messages);
+                            break;
+                        default:
+                            var errorMessage = "More than a collection of group messages was returned.";
+                            _logWriter.write("error", errorMessage);
+                            next(errorMessage, null);
+                            break;
+                    }
                 }
-            }
-        });
+            });
     };
 
     messagesManager.getPrivateMessages = function (requestingUserId, userId, next) {
 
         _logWriter.write("debug", "Getting private messages between users with Ids '" + requestingUserId + "' and '" + userId + "'...");
 
-        _conversationsRepository.list({ category: 3, userIds: [requestingUserId, userId].sort(function (a, b) { return a - b; }) }, { messages: true }, function (err, results) {
-            if (err) {
-                _logWriter.write("error", "Failed to get private messages.\n" + "Error message:\n" + "\t" + err);
-                next(err, null);
-            } else {
-                switch (results.length) {
-                    case 0:
-                        _logWriter.write("debug", "No collection was found. Returning empty array.");
-                        next(null, []);
-                        break;
-                    case 1:
-                        _logWriter.write("debug", "The collection was found. Returning the associated messages.");
-                        next(null, results[0].messages);
-                        break;
-                    default:
-                        var errorMessage = "More than a collection of private messages was returned.";
-                        _logWriter.write("error", errorMessage);
-                        next(errorMessage, null);
-                        break;
+        _conversationsRepository.list(
+            { userIds: [requestingUserId, userId].sort(function (a, b) { return a - b; }) }, // Query
+            { messages: true }, // Projection
+            function (err, results) {
+                if (err) {
+                    _logWriter.write("error", "Failed to get private messages.\n" + "Error message:\n" + "\t" + err);
+                    next(err, null);
+                } else {
+                    switch (results.length) {
+                        case 0:
+                            _logWriter.write("debug", "No collection was found. Returning empty array.");
+                            next(null, []);
+                            break;
+                        case 1:
+                            _logWriter.write("debug", "The collection was found. Returning the associated messages.");
+                            next(null, results[0].messages);
+                            break;
+                        default:
+                            var errorMessage = "More than a collection of private messages was returned.";
+                            _logWriter.write("error", errorMessage);
+                            next(errorMessage, null);
+                            break;
+                    }
                 }
-            }
-        });
+            });
     };
 
 })(module.exports)
