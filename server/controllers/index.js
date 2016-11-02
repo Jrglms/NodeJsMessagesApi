c580c9eee3103298e8f1c8178afad6ad9cd51f8b
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-11-02 20:34:22 +0000

    Created database per request

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,6 +2,7 @@
 
     controllers.init = function (app) {
 
+        var commonAppConfig = require("common/appConfig");
         var logWriter = require("../appConfig").logWriter;
 
         // Adding a line before each requests to make things more readable
@@ -13,6 +14,25 @@
         })
 
         var authenticationController = require("./authenticationController");
+
+        // Creating the database for each request
+        app.all("*", function (req, res, next) {
+
+            commonAppConfig.getDatabase(function (err, db) {
+
+                if (err) {
+                    logWriter.write("error", err);
+
+                    res.sendStatus(500);
+                }
+                else {
+                    res.locals.db = db;
+
+                    next();
+                }
+            });
+        })
+
         var messagesController = require("./messagesController");
         
         authenticationController.init(app); // Always needs to be the first one being initialized. Authentication should always be done before attending a request.
