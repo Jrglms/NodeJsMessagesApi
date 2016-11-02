89e01afd7ede8b15f002df34ceeac988aa81e806
Jorge Lomas <jorge.lomas.rodriguez+dev@gmail.com>
2016-11-02 21:02:29 +0000

    Updated repository functionallity to work with Datastore

---
+++ common/repositories/repository.js
@@ -0,0 +1,44 @@
+﻿(function (repository) {
+    
+    var _db = null;
+
+    repository.init = function (db) {
+
+        _db = db;
+    }
+    
+    repository.list = function (indentifier, next) {
+
+        var key = db.key(identifier);
+
+        _db.get(key, function (err, entity) {
+            if (err) {
+                next(err, null);
+            }
+            else {
+                next(null, entity.data);
+            }
+        });
+    };
+
+    repository.upsert = function (identifier, entity, next) {
+
+        var key = db.key(identifier);
+
+        var entityToSave = {
+            key: key,
+            data: entity
+        };
+
+        _db.save(entityToSave, function (err) {
+
+            if (err) {
+                next(err);
+            }
+            else {
+                next(null);
+            }
+        });
+    };
+
+})(module.exports)
